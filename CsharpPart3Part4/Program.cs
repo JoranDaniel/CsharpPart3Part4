8e7f8e340f401008f8a9d692f106f8ad607f335f
JoranDaniel <124999321+JoranDaniel@users.noreply.github.com>
2023-03-13 11:27:04 +0100

    exercise 3-24
    Commit 3-24

--- CsharpPart3Part4/Program.cs
+++ CsharpPart3Part4/Program.cs
@@ -1,1 +1,27 @@
+using System;
 
+class LoginProgram
+{
+    static void Main()
+    {
+        string username, password;
+
+        Console.Write("Enter your username: ");
+        username = Console.ReadLine();
+
+        Console.Write("Enter your password: ");
+        password = Console.ReadLine();
+
+        if ((username == "alex" && password == "sunshine") ||
+            (username == "emma" && password == "haskell"))
+        {
+            Console.WriteLine("Login successful.");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect username or password.");
+        }
+
+        Console.ReadLine();
+    }
+}
