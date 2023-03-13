518c1e71c204bcfb24210fa4c1758d604ea81ce9
JoranDaniel <124999321+JoranDaniel@users.noreply.github.com>
2023-03-13 12:30:42 +0100

    exercise 3-29
    exercise 3-29

--- CsharpPart3Part4/Program.cs
+++ CsharpPart3Part4/Program.cs
@@ -4,11 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        string line;
-        while ((line = Console.ReadLine()) != "")
+        string name, oldestName = "";
+        int age, oldestAge = 0;
+
+        while (true)
         {
-            string[] words = line.Split(' ');
-            Console.WriteLine(words[words.Length - 1]);
+            Console.Write("Vul je naam in (of druk op enter om te stoppen): ");
+            name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                break;
+            }
+
+            Console.Write("Vul je leeftijd in: ");
+            age = int.Parse(Console.ReadLine());
+
+            if (age > oldestAge)
+            {
+                oldestName = name;
+                oldestAge = age;
+            }
         }
+
+        if (string.IsNullOrEmpty(oldestName))
+        {
+            Console.WriteLine("Er zijn geen namen ingevoerd.");
+        }
+        else
+        {
+            Console.WriteLine("Naam van de oudste persoon: " + oldestName);
+        }
+
+        Console.WriteLine("Druk op een toets om af te sluiten.");
+        Console.ReadKey();
     }
 }
