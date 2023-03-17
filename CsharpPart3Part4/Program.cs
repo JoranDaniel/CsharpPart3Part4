a9f8f62548db0822fac03a331e71957357ad77e9
JoranDaniel <124999321+JoranDaniel@users.noreply.github.com>
2023-03-17 12:31:26 +0100

    exercise 3-31
    exercise 3-31

--- CsharpPart3Part4/Program.cs
+++ CsharpPart3Part4/Program.cs
@@ -4,39 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name, oldestName = "";
-        int age, oldestAge = 0;
+        // Current year
+        int currentYear = DateTime.Now.Year;
+
+        // Variables for longest name and highest age
+        string longestName = null;
+        int highestAge = int.MinValue;
 
         while (true)
         {
-            Console.Write("Vul je naam in: ");
-            name = Console.ReadLine();
+            Console.Write("Enter name and birth year (or empty line to finish): ");
+            string input = Console.ReadLine();
 
-            if (string.IsNullOrEmpty(name))
+            // Check if the user wants to stop entering names
+            if (string.IsNullOrWhiteSpace(input))
             {
-                break;
+                if (longestName == null && highestAge == int.MinValue)
+                {
+                    Console.WriteLine("No names or birth years entered.");
+                }
+                else
+                {
+                    Console.WriteLine("Longest name: " + longestName);
+                    Console.WriteLine("Highest age: " + highestAge);
+                }
+
+                Console.Write("Do you want to continue entering names? (y/n): ");
+                string response = Console.ReadLine().ToLower();
+                if (response != "y")
+                {
+                    break;
+                }
+
+                continue;
             }
 
-            Console.Write("Vul je leeftijd in: ");
-            age = int.Parse(Console.ReadLine());
-
-            if (age > oldestAge)
+            // Parse input into name and birth year
+            string[] parts = input.Split(',');
+            if (parts.Length != 2)
             {
-                oldestName = name;
-                oldestAge = age;
+                Console.WriteLine("Invalid input. Please enter name and birth year separated by a comma.");
+                continue;
+            }
+
+            string name = parts[0].Trim();
+            int birthYear;
+            if (!int.TryParse(parts[1].Trim(), out birthYear))
+            {
+                Console.WriteLine("Invalid input. Birth year must be an integer.");
+                continue;
+            }
+
+            // Calculate age
+            int age = currentYear - birthYear;
+
+            // Update longest name and highest age if necessary
+            if (longestName == null || name.Length > longestName.Length)
+            {
+                longestName = name;
+            }
+            if (age > highestAge)
+            {
+                highestAge = age;
             }
         }
-
-        if (string.IsNullOrEmpty(oldestName))
-        {
-            Console.WriteLine("Er zijn geen namen of leeftijden ingevoerd.");
-        }
-        else
-        {
-            Console.WriteLine("Leeftijd van het oudste persoon: " + oldestAge);
-        }
-
-        Console.WriteLine("Druk op een toets om af te sluiten.");
-        Console.ReadKey();
     }
 }
